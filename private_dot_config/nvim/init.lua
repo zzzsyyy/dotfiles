fc6e4ac909cead4e54f2246d825afe761eb1a9b0
zzzsyyy <me@zzzsy.top>
2023-04-02 21:29:02 +0800

    add: config de nvim

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,35 @@
+require("base")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local theme = require("other.utils").get_colorscheme()
+
+local opts = {
+  install = {
+    colorscheme = { theme },
+  },
+  ui = {
+    size = { width = 1.0, height = 1.0 },
+  },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+}
+
+require("lazy").setup("plugins", opts)
